511916c813cccba31f659366471d5f5a6d53ba41
Muhammed Soliman <110098092+CWD2500@users.noreply.github.com>
2026-09-03 05:13:45 -0400

    Update Projects.jsx

--- src/WebPage/Projects.jsx
+++ src/WebPage/Projects.jsx
@@ -110,6 +110,7 @@ export default function Projects() {
             <div className="dot dot-1"></div>
             <div className="dot dot-2"></div>
             <div className="dot dot-3"></div>
+            <div className="dot dot-4"></div>
           </div>
           <p>جاري تحميل المشاريع...</p>
         </div>
